d701afb5411c4b233879bcbfc616804436911f2a
darshanasam1234 <darshanasam1234@hotmail.com>
2024-03-05 17:36:01 +0000

    add provider

--- rsg/provider.tf
+++ rsg/provider.tf
@@ -20,5 +20,8 @@ provider "azurerm" {
   # Configuration options
 
   tenant_id = "32b4d603-89f1-413b-b743-fd7b6dfaa2a5"
+  client_id = "d38a5569-c90c-4527-bc7a-2d9c38e0e204"
+  subscription_id = "32b4d603-89f1-413b-b743-fd7b6dfaa2a5"
+  client_secret = "Ty48Q~BBl4aB0A5_v9BzJrmN_BIgiR8W6XH6Vc7C"
   features {}
 }
