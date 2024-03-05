0acfff92ea08a786acdda2d427e4c575b5ab8ce8
darshanasam1234 <darshanasam1234@hotmail.com>
2024-03-05 18:09:41 +0000

    add

--- rsg/main.tf
+++ rsg/main.tf
@@ -4,3 +4,9 @@ resource "azurerm_resource_group" "my_resource_group" {
   name     = var.rsgname
   location = var.rsglocation
 }
+
+
+resource "azurerm_resource_group" "my_resource_group1" {
+  name     = "Ireland"
+  location = "eastus"
+}
