eabc78653594b784f2ef7f5a24de72c86c20c503
darshanasam1234 <darshanasam1234@hotmail.com>
2024-03-07 18:29:02 +0000

    adf

--- vm/main.tf
+++ vm/main.tf
@@ -49,7 +49,7 @@ resource "azurerm_linux_virtual_machine" "ubuntuvm1" {
  
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file(azurerm_ssh_public_key.name)
+    public_key = file(data.azurerm_ssh_public_key.ssh_key.id)
   }
 
   os_disk {
