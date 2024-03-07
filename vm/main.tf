519c68b08d6b729d457330665e32f8ffe3579cab
darshanasam1234 <darshanasam1234@hotmail.com>
2024-03-07 14:11:39 +0000

    change vm network name

--- vm/main.tf
+++ vm/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "productionnetwork" {
 resource "azurerm_subnet" "prodsubnet" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.prodwindows.name
-  virtual_network_name = azurerm_virtual_network.winnetwork.name
+  virtual_network_name = azurerm_virtual_network.productionnetwork.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
