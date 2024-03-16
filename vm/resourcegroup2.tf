bfd278e1eb0ee481b21d647aed5ebf7ee8b10281
darshanasam1234 <darshanasam1234@hotmail.com>
2024-03-16 00:24:05 +0000

    aadf

--- vm/resourcegroup2.tf
+++ vm/resourcegroup2.tf
@@ -1,4 +1,4 @@
-module "resourcegrouplinuxproduction" {
+module "resourcegrouplinuxproduction2" {
     source = "./module_resourcegroup"
     strrsgname = "linuxproduction10"
     strlocation = "eastus"
